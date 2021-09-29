3637f59f477b8df94d22aaa040a4baf9b20b2e13
Vitalii Fedyk <vitalii.fedyk.it.2021@lpnu.ua>
2021-09-29 13:47:30 +0300

    4.11

---
+++ lab.4.1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int k = 1, N;
+	double S;
+
+	cout << "N="; cin >> N;
+	S = 0;
+	while (k <= N)
+	{
+		S += (1 + sqrt(1 + k * k)) / (k * k);
+		k++;
+	}
+	cout << S << endl;
+
+	k = 1;
+	S = 0;
+	do {
+		S += (1 + sqrt(1 + k * k)) / (k * k);
+		k++;
+	} while (k <= N);
+	cout << S << endl;
+
+	S = 0;
+	for (k = 1; k <= N; k++)
+	{
+		S += (1 + sqrt(1 + k * k)) / (k * k);
+	}
+	cout << S << endl;
+
+	S = 0;
+	for (k = N; k >= 1; k--)
+	{
+		S += (1 + sqrt(1 + k * k)) / (k * k);
+	}
+	cout << S;
+
+	return 0;
+}
+
+
